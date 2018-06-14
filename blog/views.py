1119e5c2051abda2efc6916929b7c115f12c280b
TAN William <william.tan@groupe-esigelec.org>
2018-06-14 14:39:09 +0200

    Test project

---
+++ blog/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from django.shortcuts import redirect
+from datetime import datetime
+
+def home(request):
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return HttpResponse("""
+        <h1>Bienvenue sur mon blog !</h1>
+        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
+    """)
+
+def view_article(request, id_article):
+    if id_article > 100:
+        raise Http404
+
+    return redirect(view_redirection)
+
+def view_redirection(request):
+    return HttpResponse("Vous avez été redirigé.")
+
+def list_articles(request, month, year):
+    """ Liste des articles d'un mois précis. """
+    return HttpResponse(
+        "Vous avez demandé les articles de {0} {1}.".format(month, year)
+    )
+
+def date_actuelle(request):
+    return render(request, 'blog/date.html', {'date': datetime.now()})
+
+
+def addition(request, nombre1, nombre2):
+    total = nombre1 + nombre2
+
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+    return render(request, 'blog/addition.html', locals())
