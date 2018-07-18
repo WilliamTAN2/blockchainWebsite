4c5d8d4c57b85b7339d5a09e8b42ce2a78efc414
TAN William <william.tan@groupe-esigelec.org>
2018-07-18 13:25:26 +0200

    a

--- scripts/exploring_graph.py
+++ scripts/exploring_graph.py
@@ -1,6 +1,7 @@
 import inputs_outputs, recent_transaction
 from collections import namedtuple
 
+
 Node_fields = ['txid', 'parent', 'average', 'number_of_nodes']
 Node = namedtuple('Node', Node_fields)
 
@@ -32,12 +33,12 @@ def exploregraph(txid, number_of_nodes):
     block = inputs_outputs.getblockfromblockhash(blockhash)
     listoftransactionid = inputs_outputs.getlistoftransactionidfromblock(block)
 
-    g = Graph
-    g.add_node(txid, None, 0, 0)
+    g = Graph()
+    g.add_node(g, txid, None, 0, 0)
 
     for child_txid in listoftransactionid:
         time = recent_transaction.gettimestampfromtxid(txid) - recent_transaction.gettimestampfromtxid(child_txid)
-        g.add_node(child_txid, txid, g.get_new_average(txid, time, number_of_nodes), number_of_nodes)
+        g.add_node(g, child_txid, txid, g.get_new_average(g, txid, time, number_of_nodes), number_of_nodes)
         exploregraph(child_txid, number_of_nodes+1)
     return g.nodes_list
 
